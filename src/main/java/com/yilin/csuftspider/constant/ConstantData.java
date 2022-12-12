d86ad40c83b77b1cd928878330f3f8de7ddf3057
yilin <1322780122@qq.com>
2022-12-12 20:45:31 +0800

    添加常量 管理敏感数据

---
+++ src/main/java/com/yilin/csuftspider/constant/ConstantData.java
@@ -0,0 +1,35 @@
+package com.yilin.csuftspider.constant;
+
+/**
+ * Title: ConstantData
+ * Description: TODO
+ * 一些敏感数据 常量 便于管理
+ * @author Yilin
+ * @version V1.0
+ * @date 2022-12-12
+ */
+public interface ConstantData {
+
+    /**
+     * 与前端约定的 sm4 加密密钥 通常是约定密钥16位  如 6666666666666666
+     */
+    String sm4Salt = "";
+
+    /**
+     *  百度orc 图片识别 ak
+     */
+
+    String baiduAk = "";
+
+    /**
+     *  百度orc 图片识别 sk
+     */
+
+    String baiduSk = "";
+
+
+
+
+
+
+}
